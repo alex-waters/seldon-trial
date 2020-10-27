ee8f322e5ddf60f1e75583ea18284fca38c5c0be
alex-waters <alexwaters613@gmail.com>
2020-10-27 16:23:51 +0000

    added a test for local container

---
+++ local_cont_test.py
@@ -0,0 +1,12 @@
+from seldon_core.seldon_client import SeldonClient
+
+notes = ['damage to a vehicle', 'hello']
+
+endpoint = "0.0.0.0:5001"
+
+sc = SeldonClient(microservice_endpoint=endpoint)
+response = sc.microservice(
+ json_data=notes,
+ method="predict")
+
+print(response.request)
